5f93da58be27c9da1454687894eb3e6632a0fec4
NitishTiwaridevstree@2023 <karan.c@devstree.in>
2026-02-20 15:29:53 +0530

    Add usercontroller for manage user control , made user route and add into the app file , made routes folder which manage the routes , chnaged asynchandlers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
 app.use(
@@ -14,5 +14,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
 
 export default app;
